693eea0249f6e0e92fecefffb83971ca4e898625
islomnumanov <islom4ik.numanov@gmail.com>
2021-06-13 18:43:18 +0900

    m

--- notes-app.js
+++ notes-app.js
@@ -1,70 +1,3 @@
-// const notes = [{
-//     title: 'My next trip',
-//     body: 'I would like to go to Switzerland'
-// }, {
-//     title: 'Habits to work on',
-//     body: 'Exercise. Eating a bit better'
-// }, {
-//     title: 'Office modifications',
-//     body: 'Get a new seat'
-// }]
-
-// // const filters = {
-// //     searchText: ''
-// // }
-
-// // const renderNotes = function (notes, filters) {
-// //     const filteredNotes = notes.filter(function (note) {
-// //         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-// //     })
-
-// //     document.querySelector('#notes').innerHTML = ''
-
-// //     filteredNotes.forEach(function (note) {
-// //         const noteEl = document.createElement('p')
-// //         noteEl.textContent = note.title
-// //         document.querySelector('#notes').appendChild(noteEl)
-// //     })
-// // }
-
-// // renderNotes(notes, filters)
-
-// const filters = {
-//     searchText: ''
-// }
-
-// const renderNotes = function (notes, filters) {
-//     const filteredNotes = notes.filter(function (note) {
-//         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-//     })
-    
-//     document.querySelector('#notes').innerHTML = ''
-
-//     filteredNotes.forEach(function (note) {
-//         const noteEl = document.createElement('p')
-//         noteEl.textContent = note.title
-//         document.querySelector('#notes').appendChild(noteEl)
-//     })
-// }
-
-
-// renderNotes(notes, )
-
-// document.querySelector('#create-note').addEventListener('click', function (e) {
-//     e.target.textContent = 'The button was clicked'
-// })
-
-// document.querySelector('#remove-all').addEventListener('click', function (e) {
-//     document.querySelectorAll('.note').forEach(function (item, index) {
-//         item.remove()
-//     })
-// })
-
-// document.querySelector('#search-text').addEventListener('input', function (e) {
-//     filters.searchText = e.target.value
-//     renderNotes(notes, filters)
-// })
-
 const notes = [{
     title: 'My next trip',
     body: 'I would like to go to Switzerland'
@@ -87,26 +20,28 @@ const renderNotes = function (notes, filters) {
 
     document.querySelector('#notes').innerHTML = ''
 
-    filteredNotes.forEach(function (item, index) {
+    filteredNotes.forEach(function (item) {
         const noteEl = document.createElement('p')
         noteEl.textContent = item.title
         document.querySelector('#notes').appendChild(noteEl)
     })
-}
 
-// look here
+}
 
 renderNotes(notes, filters)
 
+
 document.querySelector('#create-note').addEventListener('click', function (e) {
-    console.log('Button was clicked')
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#remove-all').addEventListener('click', function (e) {
+    document.querySelectorAll('.note').forEach(function (item, index) {
+        item.remove()
+    })
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
-    renderNotes(notes, filters)
-})
-
-document.querySelector('#filter-by').addEventListener('change', function (e) {
-    console.log(e.target.value)
+    renderNotes(notes, filters )
 })
