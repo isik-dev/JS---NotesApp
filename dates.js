1ea816f829f84c7334aec9c9e807209211ea1fe9
islomnumanov <islom4ik.numanov@gmail.com>
2021-06-17 14:22:12 +0900

    Date() and moment()

---
+++ dates.js
@@ -0,0 +1,35 @@
+
+// Unix Epoch - January 1st 1970 00:00:00
+// const now = new Date()
+// const timestamp = now.getTime()
+// console.log(timestamp)
+
+// const myDate = new Date(timestamp)
+// console.log(myDate.toString())
+
+// console.log(now.toString())
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minute: ${now.getMinutes()}`)
+// console.log(`Second: ${now.getSeconds()}`)
+
+
+// 1. Create two dates in the past (use string to Date)
+// 2. Get timestamps for both
+// 3. Figure out which is first and print its time (use toString)
+
+
+// const dateOne = new Date('March 2 1981 21:09:02')
+// const dateTwo = new Date('October 4 1920 03:29:01')
+
+// const timestampOne = dateOne.getTime()
+// const timestampTwo = dateTwo.getTime()
+
+
+// if (timestampOne < timestampTwo) {
+//     console.log(`${dateOne.toString()}`)
+// } else {
+//     console.log(`${dateTwo.toString()}`)
+// }
