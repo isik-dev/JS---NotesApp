2408a43084fd6750b37d1ddc46cbaa79391590a0
islomnumanov <islom4ik.numanov@gmail.com>
2021-06-17 17:23:12 +0900

    came to the last point but stuck

--- notes-app.js
+++ notes-app.js
@@ -1,7 +1,8 @@
 let notes = getSavedNotes()
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    sortBy: 'byEdited'
 }
 
 renderNotes(notes, filters)
@@ -9,11 +10,15 @@ renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
     const id = uuidv4()
+    const timestamp = moment().valueOf()
+
     
     notes.push({
         id: id,
         title: '',
-        body: ''
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
     })
     saveNotes(notes)
     location.assign(`/edit.html#${id}`)
@@ -25,7 +30,8 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 })
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
 
 window.addEventListener('storage', function (e) {
@@ -34,18 +40,3 @@ window.addEventListener('storage', function (e) {
         renderNotes(notes, filters)
     }
 })
-
-// const now = moment()
-// now.subtract(1, 'week').subtract(20, 'days')
-// console.log(now.format('MMMM Do, YYYY'))
-// console.log(now.fromNow())
-// const nowTimeStamp = now.valueOf()
-// console.log(moment(nowTimeStamp).toString())
-
-// 1. Create a new moment 
-// 2. Set the month, day, and year to your birthday
-// 3. Use format to print it in the following way: Jan 6, 1991
-
-const newMoment = moment()
-newMoment.month(2).date(14).year(1996)
-console.log(newMoment.format('MMM D, YYYY'))
--- notes-functions.js
+++ notes-functions.js
@@ -35,7 +35,7 @@ const generateNoteDOM = function (item) {
         renderNotes(notes, filters)
     })
 
-        // Setup the note tite text
+        // Setup the note title text
         if (item.title.length > 0) {
             textEl.textContent = item.title
         } else {
@@ -48,8 +48,46 @@ const generateNoteDOM = function (item) {
         return noteEl
 }
 
+// Sort your notes by one of three ways
+const sortNotes = function (notes, sortBy) {
+    if (sortBy === 'byEdited') {
+        return notes.sort(function (a, b) {
+            if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return notes.sort(function (a, b) {
+            if (a.createdAt > b.createdAt) {
+                return -1
+            } else if (a.createdAt < b.createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'alphabetical') {
+        return notes.sort(function (a, b) {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
 // Render application notes
 const renderNotes = function (notes, filters) {
+    notes = sortNotes(notes, filters.sortBy)
     const filteredNotes = notes.filter(function (item, index) {
         return item.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
@@ -66,4 +104,10 @@ const renderNotes = function (notes, filters) {
 // Save notes in localStorage
 const saveNotes = function (notes) {
     localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+
+// Generate the last edited message
+const generateLastEdited = function (timestamp) {
+    return `Last edited ${moment(timestamp).fromNow()}`
 }
