019fd24609dea49f14960d29664edb26a45ecb99
islomnumanov <islom4ik.numanov@gmail.com>
2021-06-22 19:42:43 +0900

    got the whole picture

--- notes-edit.js
+++ notes-edit.js
@@ -1,10 +1,17 @@
+// 1. Add a DOM element between the title and the body inputs (empty span)
+// 2. Set text value: Last edited 4 hours ago
+// 3. Update value on title/body/storage change
+
+
+
 const noteTitle = document.querySelector('#note-title')
+const lastEdited = document.querySelector('#last-edited')
 const noteBody = document.querySelector('#note-text')
 const removeButton = document.querySelector('#remove-note')
 
 const noteID = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find(function (note) {
+let notes = getSavedNotes()
+let note = notes.find(function (note) {
     return note.id === noteID
 })
 
@@ -14,14 +21,19 @@ if (note === undefined) {
 
 noteTitle.value = note.title
 noteBody.value = note.body
+lastEdited.textContent = generateLastEdited(note.updatedAt)
 
 noteTitle.addEventListener('input', function (e) {
     note.title = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastEdited.textContent = generateLastEdited()
     saveNotes(notes)
 })
 
 noteBody.addEventListener('input', function (e) {
     note.body = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastEdited.textContent = generateLastEdited(note.updatedAt)
     saveNotes(notes)
 })
 
@@ -31,7 +43,21 @@ removeButton.addEventListener('click', function (e) {
     location.assign('/index.html')
 })
 
-// 1. Setup input event for title
-// 2. Update note object and save notes list
-// 3. Repeat steps 1-2 for body
-// 4. Setup a remove button that removes notes and sends users back to home page
+window.addEventListener('storage', function (e) {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        note = notes.find(function (note) {
+            return note.id === noteID
+        })
+
+        if (note === undefined) {
+            location.assign('/index.html')
+        }
+
+        noteTitle.value = note.title
+        noteBody.value = note.body
+        lastEdited.textContent = generateLastEdited(note.updatedAt)
+        
+    }
+})
+
